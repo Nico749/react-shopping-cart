6fff94ffd35ed415ce0c4af1272df09b351416f9
Nico Pasqualini <pasqualinin9@gmail.com>
2022-05-11 10:45:28 +1000

    retrieving existing users

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -14,7 +14,7 @@ export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
-// export const userRequest = axios.create({
-//   baseURL: BASE_URL,
-//   header: { token: `Bearer ${TOKEN}` },
-// });
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${TOKEN}` },
+});
